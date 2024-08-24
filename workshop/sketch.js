3ef989417a4a6b859ca41e5e8d78f4bae0da5caa
Andrei Antonescu <personal@Andreis-MacBook-Pro.local>
2024-08-24 19:22:09 +0300

    workshop live update

--- workshop/sketch.js
+++ workshop/sketch.js
@@ -1,8 +1,34 @@
 let newColor
+let context, freqOne, freqTwo, gain
+
+async function setupRNBO() {
+    const audioContext = window.AudioContext || window.webkitAudioContext
+    context = new audioContext()
+
+    // load RNBO patch - it's called a "device"
+    let response = await fetch("export/workshop.export.json")
+    const myPatcher = await response.json()
+    const myDevice = await RNBO.createDevice({ context, patcher: myPatcher })
+    
+    myDevice.node.connect(context.destination)
+
+    // get parameters
+    freqOne = myDevice.parametersById.get("freqOne")
+    freqOne.value = 350.0
+
+    freqTwo = myDevice.parametersById.get("freqTwo")
+    freqTwo.value = 500.0
+
+    gain = myDevice.parametersById.get("gain")
+    gain.value = 85.0
+
+    context.suspend()
+}
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
     background(200, 100, 50)
+    setupRNBO()
 }
 
 function draw() {
@@ -21,8 +47,16 @@ function mouseMoved() {
     noStroke()
     let area = random(100)
     ellipse(random(mouseX - area, mouseX + area), random(mouseY - area, mouseY + area), random(50))
+
+    freqOne.value = map(mouseX, 0, windowWidth, 0, 5000)
+    freqTwo.value = map(mouseY, 0, windowHeight, 5000, 0) 
+
 }
 
 function keyPressed() {
     background(200, 100, 50)
+}
+
+function startSketch() {
+    context.resume()
 }
